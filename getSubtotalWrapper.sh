8a8a3f9fedf79b29f0c6ca74ce395d6ba5271ce8
CaoLei <jslhcl@gmail.com>
2014-01-06 23:20:27 +0800

    2nd version

--- getSubtotalWrapper.sh
+++ getSubtotalWrapper.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-for i in {0..1500}
+rm -f result.txt
+
+for i in {1500..1}
 do
 	lstrDate=$(date -d "$i day ago" +%Y%m/%d)
 	./getSubtotal.sh $lstrDate
 done	
+
+sed -i '/^$/d' result.txt
