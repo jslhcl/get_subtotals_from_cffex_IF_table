64bf3d12ceb6c6f0cba61fdfa758e3b2c31b62af
CaoLei <jslhcl@gmail.com>
2014-05-16 00:29:59 +0800

    get subtotal version 2

--- getSubtotal.sh
+++ getSubtotal.sh
@@ -2,7 +2,8 @@
 
 # date -d '1 day ago' 
 echo $1
-rm -f index.xml
+#rm -f index.xml	# the front end request has been changed since April 2014
+rm -f IF.xml
 
 if [ $# -eq 1 ]; then	#the number of parameters is 0 based
 	lstrDate=$1
@@ -14,13 +15,16 @@ fi
 #ofName="$ofName.txt"
 ofName="result.txt"
 
-url="http://www.cffex.com.cn/fzjy/ccpm/$lstrDate/index.xml"
+#url="http://www.cffex.com.cn/fzjy/ccpm/$lstrDate/index.xml"
+url="http://www.cffex.com.cn/fzjy/ccpm/$lstrDate/IF.xml"
 
 wget -q $url
 
-if [ ! -f index.xml ]; then
+#if [ ! -f index.xml ]; then
+if [ ! -f IF.xml ]; then
 	echo "no data in $lstrDate"
 	exit 0
 fi
 
-./parseXML.awk index.xml $ofName
+#./parseXML.awk index.xml $ofName
+./parseXML.awk IF.xml $ofName
